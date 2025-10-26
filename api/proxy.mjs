f8ce40c0714f4d4fe7657002c8fed90856029041
goenning <me@goenning.net>
2025-10-26 20:47:04 +0100

    try again

---
+++ api/proxy.mjs
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const url = req.query.url;
+
+  if (!url) {
+    res.status(400).send("NOPE!");
+    return;
+  }
+
+  try {
+    const response = await fetch(url);
+    response.headers.forEach((value, key) => res.setHeader(key, value));
+    res.status(response.status);
+    response.body.pipeTo(
+      new WritableStream({
+        write(chunk) {
+          res.write(Buffer.from(chunk));
+        },
+        close() {
+          res.end();
+        },
+      })
+    );
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
