b0427c8eeaef475f0f2a57e5c5da7aa3a8fe603b
goenning <me@goenning.net>
2025-11-07 20:07:56 +0100

    remove 403

--- api/proxy.mjs
+++ api/proxy.mjs
@@ -4,19 +4,6 @@ import https from 'https';
 export const runtime = 'nodejs';
 
 export default function handler(req, res) {
-  const origin = req.headers.origin || req.headers.referer || "";
-  const isAllowed = 
-    origin.includes('localhost') || 
-    origin.includes('127.0.0.1') || 
-    origin.includes('seogets.com') ||
-    origin.includes('chrome-extension://pejnpfcinnpmlbicfekhhcmolijdchdi') || 
-    origin.includes('chrome-extension://hinjpjakcljiacadiaigbipnnhhhpgmm')
-  
-  if (!isAllowed) {
-    res.status(403).send("Forbidden");
-    return;
-  }
-
   if (req.method === "OPTIONS") {
     res.status(204).end();
     return;
